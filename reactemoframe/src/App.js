ff35bdea3ef6524f5a005bd9e9b932072e3c1a87
ErickCampos <erick.c.modesto@gmail.com>
2021-04-18 14:20:59 -0300

    Header and footer added as components

--- reactemoframe/src/App.js
+++ reactemoframe/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 import Routes from './Routes'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
         <Routes/>
+      <Footer/>
     </div>
   );
 }
