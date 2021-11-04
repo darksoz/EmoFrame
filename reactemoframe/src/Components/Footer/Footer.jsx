445ac04324c1f0bc74b754a7d7a181601a2ea392
darksoz <suzanesantos8@gmail.com>
2021-11-04 14:15:20 -0300

    starting user register page

--- reactemoframe/src/Components/Footer/Footer.jsx
+++ reactemoframe/src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <div>
-            <footer class="bg-light text-center text-lg-start">
+            <footer class="bg-light text-center text-lg-start fixed-bottom">
                 <div class="text-center p-3 mt-4">
                     © 2021 Suzane Santos dos Santos
                 </div>
--- reactemoframe/src/Pages/UserRegister/UserRegister.jsx
+++ reactemoframe/src/Pages/UserRegister/UserRegister.jsx
@@ -1,10 +1,85 @@
 import Tabs from "../../Components/Tabs/Tabs";
+import "./UserRegister.css";
 
-function UserRegister (){
-    return(
-        <> 
-            <Tabs active="register"/>
-            <h1>User register</h1>
+function UserRegister() {
+    return (
+        <>
+            <Tabs active="register" />
+
+            <div class="wrapper">
+                <div class="container">
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-12">
+
+                                <div class="form">
+
+                                    <p class="h2">Cadastrar novo usuário</p>
+
+
+                                    <div>
+                                        <select class="form-select inputbox mt-3" aria-label="Default select example">
+                                            <option selected>Quem está respondendo esse cadastro?</option>
+                                            <option value="1">Usuário</option>
+                                            <option value="1">Responsável</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="row">
+
+                                        <div class="col-md-6">
+
+                                            <div class="inputbox mt-3 text-start"> <span>Nome </span> <br/>
+                                            <input type="text" placeholder="Nome" name="" class="form-control" /></div>
+
+                                        </div>
+
+                                        <div class="col-md-6">
+
+                                        <div class="inputbox mt-3 text-start"> <span>Sobrenome</span> <br/>
+                                        <input type="text" placeholder="Sobrenome" name="" class="form-control"/></div>
+
+                                        </div>
+
+                                        <div class="row">
+
+                                            <div class="col-md-6">
+
+                                            <div class="inputbox mt-3 text-start"> <span>Nome Social</span> <br/>
+                                            <input type="text" placeholder="Nome social" name="" class="form-control"/> </div>
+
+                                            </div>
+
+                                            <div class="col-md-6">
+
+                                            <div class="inputbox mt-3 text-start"> <span>Email</span> <br/>
+                                            <input type="text" placeholder="Email" name="" class="form-control"/> </div>
+
+                                            </div>
+
+                                        </div>
+
+                                        
+
+                                        
+
+                                    </div>
+
+
+
+                                </div>
+
+
+                            </div>
+
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+
         </>
     );
 }
