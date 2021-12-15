6f876ed70d39425fe316b06c9ce5269ae785c93a
darksoz <suzanesantos8@gmail.com>
2021-12-15 13:52:35 -0300

    Adding new questions to PANAS

--- reactemoframe/src/Pages/Panas/Panas.jsx
+++ reactemoframe/src/Pages/Panas/Panas.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './Panas.css';
 import Container from 'react-bootstrap/Container';
 import { Breadcrumb } from "react-bootstrap";
@@ -5,12 +6,49 @@ import PanasExample from '../../Components/PanasExample/PanasExample';
 import PanasForm from '../../Components/PanasForm/PanasForm';
 import arrayShuffle from 'array-shuffle';
 import { Questions1 } from "../../services/Questions/Panas/Panas.js";
+import { Questions2 } from "../../services/Questions/Panas/Panas.js";
+import { MultiStepForm, Step } from 'react-multi-form';
+import { Link } from 'react-scroll';
+import sortArray from 'sort-array';
+import { getUsername } from '../../services/auth';
+
+
 
 
 let firstQuestions = arrayShuffle(Questions1);
+let secondQuestions = arrayShuffle(Questions2);
+let amountOfQuestions = (firstQuestions.length + secondQuestions.length);
 
 
 function Panas() {
+
+    const [active, setActive] = React.useState(1);
+    const [answers, setAnswers] = React.useState([]);
+    const [title, setTitle] = React.useState("");
+    const [body, setBody] = React.useState("");
+    const [show, setShow] = React.useState(false);
+    const [success, setSuccess] = React.useState(false);
+
+    const handleChange = (event) => {
+        const id = parseInt(event.target.name);
+        const data = { id, answer: event.target.value };
+
+        if (answers.some(a => a.id === id)) {
+            setAnswers([...answers.filter(b => b.id !== id), data]);
+        }
+        else {
+            setAnswers([...answers, data]);
+        }
+    }
+
+    const handleFormData = async () => {
+        let json = { "Datetime": new Date(Date.now()), "Instrument": "leap", "Username": getUsername(), "Questions": sortArray(answers, { by: 'id', }) }
+        console.log("Json", json)
+        json = JSON.stringify(json);
+
+    }
+
+
     return (
         <>
             <Breadcrumb>
@@ -21,18 +59,68 @@ function Panas() {
 
                 <PanasExample />
 
-                {
-                    firstQuestions.map((content, index) => (
-                        <>
-                            <div style={{
-                                marginBottom: "20px",
-                            }}>
-                                <PanasForm Title={content.Title}
-                                    Name={content.Name} />
-                                <hr></hr>
-                            </div>
-                        </>
-                    ))
+                <MultiStepForm activeStep={active}>
+
+                    <Step label="Passo 1">
+                        {
+                            firstQuestions.map((content, index) => (
+                                <>
+                                    <div style={{
+                                        marginBottom: "20px",
+                                    }}>
+                                        <PanasForm Title={content.Title}
+                                            Name={content.Name} />
+                                        <hr></hr>
+                                    </div>
+                                </>
+                            ))
+                        }
+                    </Step>
+
+                    <Step label="Passo 2">
+                        {
+
+                            secondQuestions.map((content, index) => (
+                                <>
+                                    <div style={{
+                                        marginBottom: "20px",
+                                    }}>
+                                        <PanasForm Title={content.Title}
+                                            Name={content.Name} />
+                                        <hr></hr>
+                                    </div>
+                                </>
+                            ))
+                        }
+
+                    </Step>
+
+                </MultiStepForm>
+                {(active === 1 && <Link to="sample"><button class="btn whitebutton btn-lg" onClick={() => setActive(active + 1)}>Próximo</button></Link>)}
+                {(active > 1 && active !== 2 &&
+                    <div>
+                        <Link to="sample">
+                            <button class="btn whitebutton btn-lg" onClick={() => setActive(active - 1)} >Anterior</button>
+                        </Link>
+                        <Link to="sample">
+                            <button class="btn whitebutton btn-lg" onClick={() => setActive(active + 1)}>Próximo</button>
+                        </Link>
+                    </div>)
+                }
+                {(active === 2 && answers.length === amountOfQuestions) &&
+                    <div>
+                        <Link to="sample">
+                            <button class="btn whitebutton btn-lg" onClick={() => setActive(active - 1)}>Anterior</button>
+                        </Link>
+                        <button class="btn whitebutton btn-lg" onClick={() => handleFormData()}>Salvar</button>
+                    </div>
+                }
+                {(active === 2 && answers.length !== amountOfQuestions) &&
+                    <div>
+                        <Link to="sample">
+                            <button class="btn whitebutton btn-lg" onClick={() => setActive(active - 1)}>Anterior</button>
+                        </Link>
+                    </div>
                 }
             </Container>
 
--- reactemoframe/src/services/Questions/Panas/Panas.js
+++ reactemoframe/src/services/Questions/Panas/Panas.js
@@ -1,31 +1,90 @@
 export const Questions1 = [
-    {
-      "Name": "1",
-      "Title": "Estou me sentindo INTERESSADO."
-    },
   {
-      "Name": "2",
-      "Title": "Estou me sentindo PERTURBADO."
-    },
+    "Name": "1",
+    "Title": "Estou me sentindo INTERESSADO."
+  },
   {
-      "Name": "3",
-      "Title": "Estou me sentindo EXCITADO."
-    },
+    "Name": "2",
+    "Title": "Estou me sentindo PERTURBADO."
+  },
   {
-      "Name": "4",
-      "Title": "Estou me sentindo ATORMENTADO."
-    },
+    "Name": "3",
+    "Title": "Estou me sentindo EXCITADO."
+  },
   {
-      "Name": "5",
-      "Title": "Estou me sentindo AGRADAVELMENTE."
-    },
+    "Name": "4",
+    "Title": "Estou me sentindo ATORMENTADO."
+  },
   {
-      "Name": "6",
-      "Title": "Estou me sentindo CULPADO."
-    },
+    "Name": "5",
+    "Title": "Estou me sentindo AGRADAVELMENTE SURPREENDIDO."
+  },
   {
-      "Name": "7",
-      "Title": "Estou me sentindo ASSUSTADO."
-    }
-  
-  ];
+    "Name": "6",
+    "Title": "Estou me sentindo CULPADO."
+  },
+  {
+    "Name": "7",
+    "Title": "Estou me sentindo ASSUSTADO."
+  },
+
+  {
+    "Name": "8",
+    "Title": "Estou me sentindo CALOROSO."
+  },
+
+  {
+    "Name": "9",
+    "Title": "Estou sentindo REPULSA."
+  },
+
+  {
+    "Name": "10",
+    "Title": "Estou me sentindo ENTUSIASMADO."
+  }
+];
+
+export const Questions2 = [
+  {
+    "Name": "11",
+    "Title": "Estou me sentindo ORGULHOSO."
+  },
+
+  {
+    "Name": "12",
+    "Title": "Estou me sentindo IRRITADO."
+  },
+
+  {
+    "Name": "13",
+    "Title": "Estou me sentindo ENCANTADO"
+  },
+  {
+    "Name": "14",
+    "Title": "Estou sentindo REMORSO."
+  },
+  {
+    "Name": "15",
+    "Title": "Estou me sentindo INSPIRADO."
+  },
+  {
+    "Name": "16",
+    "Title": "Estou me sentindo NERVOSO."
+  },
+  {
+    "Name": "17",
+    "Title": "Estou me sentindo DETERMINADO."
+  },
+  {
+    "Name": "18",
+    "Title": "Estou me sentindo TRÊMULO."
+  },
+  {
+    "Name": "19",
+    "Title": "Estou me sentindo ATIVO."
+  },
+  {
+    "Name": "20",
+    "Title": "Estou me sentindo AMEDRONTADO."
+  }
+];
