d8092b125a45cc46ed9e9af954d5f10098fd0862
darksoz <suzanesantos8@gmail.com>
2021-12-29 21:39:07 -0300

    creating specialist register component

--- reactemoframe/src/Pages/Register/Register.jsx
+++ reactemoframe/src/Pages/Register/Register.jsx
@@ -1,87 +1,19 @@
 import React, { useState } from 'react';
 import './Register.css';
 import Tabs from '../../Components/Tabs/Tabs';
+import RegisterSpecialist from '../../Components/RegisterSpecialist/RegisterSpecialist';
 
 function Register() {
-    
-    
+
+
     return (
         <div>
-            
-            <Tabs active="register"/>
+
+            <Tabs active="register" />
             <div className="container card mt-3">
                 <h3>Cadastre-se</h3>
-                <form class="form-horizontal">
-
-                <div className="form-group">
-                        <label class="col-sm-10 control-label text-start">Nome</label>
-                        <input type="text" className="form-control" placeholder="Nome" />
-                    </div>
-
-                    <div className="form-group">
-                        <label class="col-sm-10 control-label text-start">Sobrenome</label>
-                        <input type="text" className="form-control" placeholder="Sobrenome" />
-                    </div>
-
-                    <div className="form-group">
-                        <label class="col-sm-10 text-start control-label">Nome Social</label>
-                        <input type="text" className="form-control" placeholder="Nome social" />
-                    </div>
-
-                    <div className="form-group">
-                        <label class="col-sm-10 text-start control-label">Especialidade</label>
-                        <select class="form-select"
-                        id="especialidade"
-                        aria-label="Default select example">
-                            <option value=""  selected> Selecione a especialidade</option>
-                            <option value="0">Gerontologia</option>
-                            <option value="2">Psicologia</option>
-                            <option value="3">Fsioterapia</option>
-                            <option value="4">Terapia Ocupacional</option>
-                            <option value="5">Computação</option>
-                            <option value="1">Outra</option>
-                          </select> 
-
-                         
-                    </div>
-
-                    <div className="form-group">
-                        <label class="col-sm-10 text-start control-label">Vínculo</label>
-                        <input type="text" className="form-control" placeholder="Instituição a qual está vinculado(a)" />
-                    </div>
-
-                    <div className="form-group">
-                        <label class="col-sm-10 text-start control-label">Telefone</label>
-                        <input type="email" className="form-control" placeholder="Número do telefone" />
-                    </div>
-
-                    <div className="form-group">
-                        <label class="col-sm-10 text-start control-label">Gênero</label>
-                        <select class="form-select" aria-label="Default select example">
-                            <option selected>Selecione o gênero</option>
-                            <option value="1">Feminino</option>
-                            <option value="2">Masculino</option>
-                            <option value="3"> Não sei/Prefiro não dizer</option>
-                            <option value="4">Outro</option>
-                        </select>
-                    </div>
-
-                    <div className="form-group">
-                        <label class="col-sm-10 text-start control-label">E-mail</label>
-                        <input type="email" className="form-control" placeholder="E-mail" />
-                    </div>
-
-                    <div className="form-group">
-                        <label class="col-sm-10 text-start control-label">Senha</label>
-                        <input type="password" className="form-control" placeholder="Senha" />
-                    </div>
-
-                    <button type="submit" className="btn whitebutton btn-lg btn-block">Cadastrar</button>
-
-
-
-                </form>
-            </div>          
+                <RegisterSpecialist/>
+            </div>
         </div>
     )
 }
