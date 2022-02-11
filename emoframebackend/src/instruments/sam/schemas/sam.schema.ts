27ddeecc828e098f5ce5d434c155090fc0669f98
rafael <rafaelpx8812@gmail.com>
2022-02-11 18:27:30 -0300

    🎨 page funcionando

--- emoframebackend/src/instruments/sam/schemas/sam.schema.ts
+++
@@ -1,8 +0,0 @@
-import * as mongoose  from 'mongoose';
-
-export const SamSchema = new mongoose.Schema({
-    Datetime: String,
-    Instrument: String,
-    Questions: Array<Object>({id: String, answer: Number}),
-    Username: String,
-})
--- emoframebackend/src/token/token.ts
+++
@@ -1,6 +0,0 @@
-import {Document} from 'mongoose';
-
-export class Token extends Document{
-    hash: String;
-    email: String;
-}
