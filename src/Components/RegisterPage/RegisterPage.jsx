8bd6c9b5950634f0578affa003b06a8e1fde02aa
SUZANE SANTOS SANTOS <suzanesantos8@gmail.com>
2022-03-22 13:43:39 -0300

    fixing register page

--- src/Components/RegisterPage/RegisterPage.jsx
+++ src/Components/RegisterPage/RegisterPage.jsx
@@ -1,9 +1,43 @@
+import { useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 
 function RegisterPage() {
+
+    const [retVisibility, setRetVisibility] = useState(null);
+
+    const [workVisibility, setWorkVisibility] = useState(false);
+
+
+    const Retirement = () => {
+        var e = document.getElementById("Aposentado");
+        var strUser = e.value;
+        if (strUser === "Sim") {
+            setRetVisibility(true);
+        }
+
+        else if (strUser === "Nada") {
+            setRetVisibility(null);
+        }
+        else {
+            setRetVisibility(false);
+        }
+    }
+
+    const Remunaration = () => {
+        var e = document.getElementById("Remunerado");
+        var strUser = e.value;
+        if (strUser === "Sim") {
+            setWorkVisibility(true);
+        }
+        else {
+            setWorkVisibility(false);
+        }
+    }
+
+
     return (
         <>
             <Container className='border p-2'>
@@ -22,7 +56,7 @@ function RegisterPage() {
                                 <Col md={12}>
                                     <Form.Group className="mb-3" controlId="">
                                         <Form.Label>Serviço/Instituição</Form.Label>
-                                        <Form.Control type="text" placeholder="Serviço/Instituição" />
+                                        <Form.Control type="text" name="Instituição" placeholder="Serviço/Instituição" />
                                     </Form.Group>
                                 </Col>
                             </Row>
@@ -30,15 +64,15 @@ function RegisterPage() {
                                 <Col md={6}>
                                     <Form.Group className="mb-3" controlId="">
                                         <Form.Label>Entrada no Serviço</Form.Label>
-                                        <Form.Control type="date" />
+                                        <Form.Control type="date" name="Entrada"/>
                                     </Form.Group>
                                 </Col>
                                 <Col md={6}>
                                     <Form.Label>Entrevistado</Form.Label>
                                     <Form.Select aria-label="Default select example">
                                         <option>Quem está sendo entrevistado</option>
-                                        <option value="1">Idoso</option>
-                                        <option value="2">Outro</option>
+                                        <option value="Idoso">Idoso</option>
+                                        <option value="Outro">Outro</option>
                                     </Form.Select>
                                 </Col>
                             </Row>
@@ -46,13 +80,13 @@ function RegisterPage() {
                                 <Col md={6}>
                                     <Form.Group className="mb-3" controlId="">
                                         <Form.Label>Entrevistador</Form.Label>
-                                        <Form.Control type="text" placeholder="Entrevistador" />
+                                        <Form.Control type="text" name="entrevistador "placeholder="Entrevistador" />
                                     </Form.Group>
                                 </Col>
                                 <Col md={6}>
                                     <Form.Group className="mb-3" controlId="">
                                         <Form.Label>Data da Avaliação</Form.Label>
-                                        <Form.Control type="date" />
+                                        <Form.Control type="date" name="DataAvaliação" />
                                     </Form.Group>
                                 </Col>
                             </Row>
@@ -70,7 +104,7 @@ function RegisterPage() {
                         <Col md={12}>
                             <Form.Group className="mb-3 mt-2" controlId="">
                                 <Form.Label>Nome</Form.Label>
-                                <Form.Control type="text" placeholder="Nome" />
+                                <Form.Control type="text" name="nomepage" placeholder="Nome" />
                             </Form.Group>
                         </Col>
                     </Row>
@@ -79,16 +113,16 @@ function RegisterPage() {
                         <Col md={6}>
                             <Form.Group className="mb-3 mt-2" controlId="">
                                 <Form.Label>Idade</Form.Label>
-                                <Form.Control type="text" placeholder="Idade" />
+                                <Form.Control type="text" placeholder="Idade" name="idade" />
                             </Form.Group>
                         </Col>
                         <Col md={6}>
                             <Form.Select aria-label="Default select example">
                                 <option>Estado Cívil</option>
-                                <option value="1">Solteiro(a)</option>
-                                <option value="2">Casado(a)/União Estável</option>
-                                <option value="3">Separado(a)/Divorciado(a)</option>
-                                <option value="4">Viúvo(a)</option>
+                                <option value="Solteiro">Solteiro(a)</option>
+                                <option value="Casado">Casado(a)/União Estável</option>
+                                <option value="Separado">Separado(a)/Divorciado(a)</option>
+                                <option value="Viúvo">Viúvo(a)</option>
 
                             </Form.Select>
                         </Col>
@@ -97,21 +131,49 @@ function RegisterPage() {
                     <Row>
                         <Col md={12}>
                             <Form.Label> O(a) Sr(a) é aposentado(a) ou pensionista? </Form.Label>
-                            <Form.Select aria-label="Default select example">
-                                <option>Selecione a resposta</option>
-                                <option value="1">Sim</option>
-                                <option value="2">Não</option>
+                            <Form.Select id="Aposentado" name="RetirementOption" aria-label="Default select example" onChange={() => Retirement()}>
+                                <option disabled selected value="Nada">Selecione a resposta</option>
+                                <option value="Sim">Sim</option>
+                                <option value="Não">Não</option>
                             </Form.Select>
+                            {
+                                retVisibility === true &&
+                                <>
+                                    <Form.Group className="mb-3 mt-2" controlId="">
+                                        <Form.Label>Qual a profissão que exerceu por mais tempo?</Form.Label>
+                                        <Form.Control type="text" placeholder="Profissão exercida por mais tempo" />
+                                    </Form.Group>
+                                </>
+                            }
+                            {
+                                retVisibility === false &&
+                                <>
+                                    <Form.Group className="mb-3 mt-2" controlId="">
+                                        <Form.Label>Qual é a sua profissão atual?</Form.Label>
+                                        <Form.Control type="text" placeholder="Profissão atual" />
+                                    </Form.Group>
+                                </>
+                            }
+
                         </Col>
                     </Row>
                     <Row>
                         <Col md={12}>
                             <Form.Label> Exerce atualmente algum tipo de trabalho remunerado?? </Form.Label>
-                            <Form.Select aria-label="Default select example">
-                                <option>Selecione a resposta</option>
-                                <option value="1">Sim</option>
-                                <option value="2">Não</option>
+                            <Form.Select id="Remunerado" name="WorkOptions" aria-label="Default select example" onChange={() => Remunaration()}>
+                                <option disabled selected>Selecione a resposta</option>
+                                <option value="Sim">Sim</option>
+                                <option value="Não">Não</option>
                             </Form.Select>
+                            {
+                                workVisibility === true &&
+                                <>
+                                    <Form.Group className="mb-3 mt-2" controlId="">
+                                        <Form.Label>Qual trabalho reunerado exerce?</Form.Label>
+                                        <Form.Control type="text" placeholder="Trabalho remunerado" name="trabalhoremunerado" />
+                                    </Form.Group>
+                                </>
+                            }
                         </Col>
                     </Row>
                 </Form>
@@ -124,7 +186,7 @@ function RegisterPage() {
                 <Form>
                     <Form.Group className="mb-3 mt-2" controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Relatar como foi(foram) as últimas consultas médicas
-</Form.Label>
+                        </Form.Label>
                         <Form.Control as="textarea" rows={3} />
                     </Form.Group>
                 </Form>
