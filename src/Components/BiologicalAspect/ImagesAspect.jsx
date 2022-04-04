82dce6df49337e204e23aebfafb59c187e3b4dd9
rafael <rafaelpx8812@gmail.com>
2022-04-04 18:05:17 -0300

    📝 ajuste na questão 4

--- src/Components/BiologicalAspect/ImagesAspect.jsx
+++ src/Components/BiologicalAspect/ImagesAspect.jsx
@@ -1,18 +1,25 @@
+import { Checkbox } from "@material-ui/core";
 import { React } from "react";
 
 function ImagesAspect(props) {
   return (
     <>
-      <ul>
-        {" "}
+      <div style={{ alignItens: "flex-start" }}>
         <img src={`${process.env.PUBLIC_URL}/` + props.images} alt="" />
-        <textarea
-          class="form-control"
-          id="exampleFormControlTextarea1"
-          name={props.question + "." + props.name}
-          rows="1"
-        ></textarea>
-      </ul>
+        {props.question == 3 ? (
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            name={props.question + "." + props.name}
+            rows="1"
+          ></textarea>
+        ) : (
+          <Checkbox
+            name={props.question + "." + props.name}
+            value="1"
+          ></Checkbox>
+        )}
+      </div>
     </>
   );
 }
