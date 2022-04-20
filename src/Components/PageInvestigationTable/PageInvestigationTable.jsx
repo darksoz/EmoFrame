91f211074d285f11a09fdd05564b1e55f9298ca1
rapaix <rafaelpx8812@gmail.com>
2022-04-19 23:55:06 -0300

    ajuste na tabela

--- src/Components/PageInvestigationTable/PageInvestigationTable.jsx
+++ src/Components/PageInvestigationTable/PageInvestigationTable.jsx
@@ -1,37 +1,34 @@
 import { Table } from "react-bootstrap";
 
 function PageInvestigationTable(props) {
-    return (
-        <>
-            <div className="border border-dark bg-lgrey text-white">
-                <h3>Investigação de Demandas</h3>
-            </div>
+  return (
+    <>
+      <div className="border border-dark bg-lgrey text-white">
+        <h3>Investigação de Demandas</h3>
+      </div>
 
-            <div className="border mb-3">
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Demanda</th>
-                        <th>Aspecto</th>
-                        <th>Necessita de Investigação?</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.aspects.map((item, index) => (
-
-                        <tr key={index}>
-                            <td>{item.id}</td>
-                            <td>{item.aspect}</td>
-                            <td>{item.answer}</td>
-                        </tr>
-                    ))}
-                    
-
-                </tbody>
-            </Table>
-            </div>
-        </>
-    )
+      <div className="border mb-3">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Demanda</th>
+              <th>Aspecto</th>
+              <th>Necessita de Investigação?</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.aspects.map((item, index) =>
+              'aspect' in item && (
+                <tr key={index}>
+                  <td>{item.id}</td>
+                  <td>{item.aspect}</td>
+                  <td>{item.answer}</td>
+                </tr>
+              )) }
+          </tbody>
+        </Table>
+      </div>
+    </>
+  );
 }
 export default PageInvestigationTable;
