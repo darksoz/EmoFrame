b1d6d75a33eef42e131c3da4e473534764880688
SUZANE SANTOS SANTOS <suzanesantos8@gmail.com>
2022-05-02 11:44:46 -0300

    adding scales on poms result

--- src/Components/LineChartPoms/LineChartPoms.jsx
+++ src/Components/LineChartPoms/LineChartPoms.jsx
@@ -1,12 +1,46 @@
 import ReactApexChart from "react-apexcharts";
 
 function LineChartPoms () {  
-  
+    let dados = {
+        series: [{
+          name: "Desktops",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+      }],
+        chart: {
+        height: 350,
+        type: 'line',
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'straight'
+      },
+      title: {
+        text: 'Product Trends by Month',
+        align: 'left'
+      },
+      grid: {
+        row: {
+          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+          opacity: 0.5
+        },
+      },
+      xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+      }
+      };
     return (
         <>
-        fazer depois
+        <ReactApexChart options={dados.options} 
+                            series={dados.series} 
+                            type="line" 
+                            height={350} />
         </>
-    );
+    )
 }
 
 export default LineChartPoms;
--- src/Pages/PomsResult/PomsResult.jsx
+++ src/Pages/PomsResult/PomsResult.jsx
@@ -1,8 +1,15 @@
-import { Breadcrumb, Container, Button, Row, Col } from "react-bootstrap";
+import { Breadcrumb, Container, Row, Col } from "react-bootstrap";
 import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header";
-import LineChartPoms from "../../Components/LineChartPoms/LineChartPoms";
 import PomsTable from "../../Components/PomsTable/PomsTable";
+
+let T_scale = [1, 10, 14, 18, 26, 18];
+let D_scale = [6, 13, 15, 19, 27, 33];
+let H_scale = [2, 8, 12, 22, 25, 32];
+let F_scale = [3, 11, 17, 20, 30, 36];
+let V_scale = [4, 7, 9, 24, 29, 34];
+let C_scale = [5, 16, 21, 23, 31, 35];
+let Reverse = [23, 26, 31];
 
 function PomsResult() {
     return (
@@ -17,35 +24,25 @@ function PomsResult() {
 
                 <Row class="mt-5">
                     <Col>
-                    <PomsTable/>
+                        <PomsTable />
                     </Col>
                 </Row>
-               
+
                 <Row>
 
-                <h5>Fórmula de Perturbação Total de Humor (PTH)</h5>
+                    <h5>Fórmula de Perturbação Total de Humor (PTH)</h5>
 
                     <Col>
 
-        
-                        <div class="text-center border border-dark p-4"> 
-        
-                          <h3>PTH =[(tensão + depressão + hostilidade + fadiga + confusão)- vigor]+ 100 = X
-                        </h3>  
+
+                        <div class="text-center border border-dark p-4">
+
+                            <h3>PTH =[(tensão + depressão + hostilidade + fadiga + confusão)- vigor]+ 100 = X
+                            </h3>
                         </div>
                         <p>Quanto maior o valor de PTH, maior é a piora do humor.</p>
                     </Col>
                 </Row>
-
-                <Row>
-                    <Col>
-                    <LineChartPoms/>
-                    </Col>
-                </Row>
-
-               
-
-              
             </Container>
             <Footer />
         </>
