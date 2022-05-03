9583ef151e19bc43ac975a12a027f0d10abb78e3
SUZANE SANTOS SANTOS <suzanesantos8@gmail.com>
2022-05-03 19:30:41 -0300

    going back to previous landing page

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,16 +5,9 @@ import Col from 'react-bootstrap/Col';
 import { Container } from 'react-bootstrap';
 
 
-
-
-
 function Footer() {
     return (
-        <>
-            {
-                (window.location.pathname !== "/" && window.location.pathname !== "/dashboard")  &&
                 <>
-
                     <Container fluid>
                         <div className='margin_top'>
 
@@ -39,57 +32,12 @@ function Footer() {
                                         <span>
                                             <a href="/contactus" class="link-dark"> &nbsp;Sobre Nós</a>
                                         </span>
-
                                     </Col>
                                 </Row>
-
-
                             </footer>
                         </div>
-
-
                     </Container>
                 </>
-            }
-            {
-                (window.location.pathname === "/" || window.location.pathname === "/dashboard") &&
-                <>
-
-                    <Container fluid>
-
-                            <footer class="text-center text-lg-start mt-3">
-
-                                <Row>
-                                    <Col lg={9}>
-                                        <div>
-                                            <img
-                                                src={`${process.env.PUBLIC_URL}/emoframe.png`}
-                                                width="30"
-                                                className="d-inline-block align-center"
-                                                alt=""
-                                            />
-                                            © 2022 Suzane Santos dos Santos
-                                        </div>
-                                    </Col>
-                                    <Col lg={3} className='text-end'>
-                                        <span>
-                                            <a href="/contactus" class="link-dark">Contato &nbsp;</a>
-                                        </span>
-                                        <span>
-                                            <a href="/contactus" class="link-dark"> &nbsp;Sobre Nós</a>
-                                        </span>
-
-                                    </Col>
-                                </Row>
-
-
-                            </footer>
-
-
-                    </Container>
-                </>
-            }
-        </>
 
     )
 }
--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Figure, Row } from 'react-bootstrap';
 import './LandingPage.css';
 import React from 'react';
 import Footer from '../../Components/Footer/Footer';
@@ -6,75 +6,33 @@ import { RediretToPage } from '../../services/utils';
 import Link from 'react-scroll/modules/components/Link';
 import { isAuthenticated } from '../../services/auth';
 import { Redirect } from 'react-router-dom';
+import Header from '../../Components/Header/Header';
 
 function LandingPage() {
 
-  
-    return isAuthenticated() ? <Redirect to='/dashboard'/> :(
+
+    return isAuthenticated() ? <Redirect to='/dashboard' /> : (
         <>
-            <section class="masthead d-flex align-items-center" id="page-top">
+        <Header/>
+            <Container className='mt-5'>
+                <Row>
+                    <Col className='p-2'>
+                        <img src={`${process.env.PUBLIC_URL}/emoframe.png`} className="mt-5 mb-3" style={{ width: "350px" }} alt="Responsive" />
 
-                <Container className='mt-5'>
-                    <Row className='mt-5'>
-                        <Col className='mt-5 align-items-center'>
+                        <blockquote>
+                            <p class="lead"> Um framework composto por Instrumentos de Avaliação de Respostas Emocionais </p>
+                        </blockquote>
+                        <div class="d-flex justify-content-center">
 
-                            <span>                
-                                <button class="btn btn-dark btn-xl" onClick={() => RediretToPage('login')}>Entrar</button>
-                            </span>
+                            <button class="btn whitebutton btn-lg" onClick={() => RediretToPage('login')}>Entrar</button>
 
-                            <span>                
-                                <Link style={{marginLeft: "1em"}} class="btn btn-dark btn-xl" to="services">Saiba Mais</Link>
-                            </span>
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-
-            <section className='about' id="services">
-                
-                <Container className='mb-2'>
-                    <Row>
-                        <p className='h1'> Sobre o EmoFrame</p>
-                    </Row>
-
-                    <Row>
-                        <h3>Analisar aspectos emocionais de usuários, quando estes interagem com soluções computacionais interativas, constitui-se um desafio para profissionais da Computação. Em diversas situações, tal avaliação fica a cargo dos profissionais especialistas do domínio estudado. Este projeto busca reunir diferentes instrumentos para a avaliação de respostas emocionais em um framework, o EmoFrame.
-                        </h3>
-                    </Row>
-
-                    <Row className='mt-5'>
-
-                        <h4> O EmoFrame tem como função</h4>
-
-                        <Col>
-                            <Card className='bgA'>
-
-                                <p className='lead'>Auxiliar o profissional da Computação a encontrar ferramentas adequadas para o público alvo da sua aplicação, levando em consideração as particularidades dos usuários.</p>
-                            </Card>
-                        </Col>
-
-                        <Col >
-                            <Card className='bgB'>
-                                <p className='lead'> Auxiliar profissionais, sobretudo da área da Saúde, a aplicarem seus instrumentos de forma sistematizada e com rápida visualização dos resultados. </p>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card className='bgC'>
-                                <p className='lead'> Viabilizar que profissionais da Computação, também possam usar instrumentos de outros domínios de modo a obter resultados sobre a efetividade no uso da sua solução. </p>
-                            </Card>
-
-                        </Col>
-                    </Row>
-                </Container>
-                <button class="btn whitebutton btn-lg btn-block" onClick={() => RediretToPage('recommendation')}>Encontre o instrumento adequado para sua solução</button>
-            </section>
-
-            <Link class="scroll-to-top rounded" to="page-top"><i class="fas fa-lg fa-angle-up"></i></Link>
+                            <Link style={{ marginLeft: "1em" }} class="btn whitebutton btn-lg" to="services">Saiba Mais</Link>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
 
             <Footer />
-
-
-
 
         </>
 
